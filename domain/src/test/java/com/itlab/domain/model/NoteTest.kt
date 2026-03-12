6d1484b6a348f02246b63df8e9188d227dcb2a80
Никита Смирнов <152095229+N1smi@users.noreply.github.com>
2026-03-12 18:09:33 +0300

    test: add NoteTest file

---
+++ domain/src/test/java/com/itlab/domain/model/NoteTest.kt
@@ -0,0 +1,12 @@
+package com.itlab.domain
+
+import com.itlab.domain.model.Note
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class NoteTest {
+    @Test
+    fun defaultNoteConstructor() {
+        
+    }
+}
