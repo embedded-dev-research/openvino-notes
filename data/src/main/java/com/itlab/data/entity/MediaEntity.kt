4ecd19e1cc3efe03371f7c5f42aefd3f045baca7
sannarat <sannarat45@gmail.com>
2026-03-17 08:55:23 +0300

    style: fixed lint

--- data/src/main/java/com/itlab/data/entity/MediaEntity.kt
+++ data/src/main/java/com/itlab/data/entity/MediaEntity.kt
@@ -1,28 +1,27 @@
 package com.itlab.data.entity
 
 import androidx.room.Entity
-import androidx.room.PrimaryKey
 import androidx.room.ForeignKey
-import kotlinx.datetime.Clock
-import kotlinx.datetime.Instant
+import androidx.room.PrimaryKey
 
 @Entity(
     tableName = "media",
-    foreignKeys  = [ForeignKey(
-        entity = NoteEntity::class,
-        parentColumns = ["id"],
-        childColumns = ["noteId"],
-        onDelete = ForeignKey.CASCADE
-    )]
+    foreignKeys = [
+        ForeignKey(
+            entity = NoteEntity::class,
+            parentColumns = ["id"],
+            childColumns = ["noteId"],
+            onDelete = ForeignKey.CASCADE,
+        ),
+    ],
 )
-
 data class MediaEntity(
     @PrimaryKey
-    val id : String,
-    val noteId : String,
-    val type : String,
-    val remoteUrl : String,
-    val localPath : String?,
-    val mimeType : String,
-    val size : Long? = null
+    val id: String,
+    val noteId: String,
+    val type: String,
+    val remoteUrl: String,
+    val localPath: String?,
+    val mimeType: String,
+    val size: Long? = null,
 )
