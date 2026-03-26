3708402aa353d92bcc11492c7c10c7fef2aa8ea1
IntFxZen <ilablohin74@gmail.com>
2026-03-26 19:02:42 +0300

    Update editor

--- app/src/main/java/com/itlab/notes/ui/editor/EditorScreen.kt
+++ app/src/main/java/com/itlab/notes/ui/editor/EditorScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
