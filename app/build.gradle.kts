f5ea2bc763535ee4c4878faf0d49607a517b2593
sannarat <sannarat45@gmail.com>
2026-04-09 20:20:27 +0200

    fix: fix ci

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     defaultConfig {
         applicationId = "com.itlab.notes"
         minSdk = 33
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
