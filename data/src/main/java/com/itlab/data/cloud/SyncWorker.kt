fd58bd2662d34dab1b8d17150740cd192a8a654a
sannarat <sannarat45@gmail.com>
2026-04-16 20:38:14 +0200

    feat: add class SynkWorker

--- data/src/main/java/com/itlab/data/cloud/SyncWorker.kt
+++ data/src/main/java/com/itlab/data/cloud/SyncWorker.kt
@@ -1,8 +1,34 @@
 package com.itlab.data.cloud
 
 import android.content.Context
-import android.util.Log
+import timber.log.Timber
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.itlab.domain.cloud.SyncManager
 import com.itlab.data.cloud.AuthManager
+
+class SyncWorker(
+    context: Context,
+    params: WorkerParameters,
+    private val syncManager: SyncManager,
+    private val authManager: AuthManager
+) : CoroutineWorker(context, params) {
+
+    override suspend fun doWork(): Result {
+        val userId = authManager.getCurrentUserId() ?: run {
+            Timber.e("Sync failed: User is not authorized")
+            return Result.failure()
+        }
+        return try {
+            Timber.d("Starting sync for user: $userId")
+            syncManager.sync(userId)
+
+            Timber.d("Sync completed successfully")
+            Result.success()
+        }catch (e: Exception){
+            Timber.e(e, "Sync error occurred: %s", e.message)
+
+            if (e is java.io.IOException) Result.retry() else Result.failure()
+        }
+    }
+}
