e4676b04138fd66f801fe9343d874f8a076024b9
sannarat <sannarat45@gmail.com>
2026-04-16 20:10:37 +0200

    feat: add new dep

---
+++ data/src/main/java/com/itlab/data/cloud/SyncWorker.kt
@@ -0,0 +1,8 @@
+package com.itlab.data.cloud
+
+import android.content.Context
+import android.util.Log
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.itlab.domain.cloud.SyncManager
+import com.itlab.data.cloud.AuthManager
